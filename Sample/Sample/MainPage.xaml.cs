80f66494311b7946e14922e9082afa4ab50d6cc5
leonard <leojharris@hotmail.com>
2018-01-30 17:01:49 +1300

    .

--- Sample/Sample/MainPage.xaml.cs
+++ Sample/Sample/MainPage.xaml.cs
@@ -29,19 +29,20 @@ namespace Sample
             };
 
             double GetRoundedLabelWidth(RelativeLayout parent) =>
-                roundedLabelView.Measure(parent.Width, parent.Height).Request.Width;
+                roundedLabelView.Measure(parent.Width, parent.Height).Request.Width + 80;
 
             double GetRoundedLabelHeight(RelativeLayout parent) =>
-              roundedLabelView.Measure(parent.Width, parent.Height).Request.Height;
+              roundedLabelView.Measure(parent.Width, parent.Height).Request.Height + 30;
 
             relativeLayout.Children.Add(
                          roundedLabelView,
-                         Constraint.RelativeToParent(parent => parent.Width / 2 - (GetRoundedLabelWidth(parent) + 60) / 2),
+                         Constraint.RelativeToParent(parent => parent.Width / 2 - (GetRoundedLabelWidth(parent)) / 2),
                           Constraint.Constant(0),//Constraint.RelativeToParent(parent => parent.Height / 2 - (GetRoundedLabelHeight(parent) + 30) / 2),
-                         Constraint.RelativeToParent(layout => GetRoundedLabelWidth(layout) + 60)
+                         Constraint.RelativeToParent(layout => GetRoundedLabelWidth(layout))
                          //,Constraint.RelativeToParent(layout => GetRoundedLabelHeight(layout) + 30)
                          );
-
+            relativeLayout.Padding = new Thickness(80,0);
+           
             RoundedLabelView.Content = relativeLayout;
         }
     }
