715d5976b317ead87e249f05291274c070be1339
leonard <leojharris@hotmail.com>
2018-01-30 17:13:53 +1300

    Fixes from merge

--- Sample/Sample/MainPage.xaml.cs
+++ Sample/Sample/MainPage.xaml.cs
@@ -8,8 +8,8 @@ namespace Sample
         public MainPage()
         {
             InitializeComponent();
-            
-            OnlyRoundedLabelView.Content = new RoundedLabel
+
+            var roundedLabel = new RoundedLabel
             {
                 TextColor = Color.White,
                 CurvedBackgroundColor = Color.Red,
@@ -19,41 +19,29 @@ namespace Sample
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand,
                 VerticalTextAlignment = TextAlignment.Center,
-               // HeightRequest = 0,
+                // HeightRequest = 0,
                 FontSize = 15,
                 FontAttributes = FontAttributes.Bold
             };
 
+            var relativeLayout = new RelativeLayout();
+
             double GetRoundedLabelWidth(RelativeLayout parent) =>
-                roundedLabelView.Measure(parent.Width, parent.Height).Request.Width + 80;
+                roundedLabel.Measure(parent.Width, parent.Height).Request.Width + 80;
 
             double GetRoundedLabelHeight(RelativeLayout parent) =>
-              roundedLabelView.Measure(parent.Width, parent.Height).Request.Height + 30;
+              roundedLabel.Measure(parent.Width, parent.Height).Request.Height + 30;
 
             relativeLayout.Children.Add(
-                         roundedLabelView,
+                         roundedLabel,
                          Constraint.RelativeToParent(parent => parent.Width / 2 - (GetRoundedLabelWidth(parent)) / 2),
                           Constraint.Constant(0),//Constraint.RelativeToParent(parent => parent.Height / 2 - (GetRoundedLabelHeight(parent) + 30) / 2),
                          Constraint.RelativeToParent(layout => GetRoundedLabelWidth(layout))
                          //,Constraint.RelativeToParent(layout => GetRoundedLabelHeight(layout) + 30)
                          );
-            relativeLayout.Padding = new Thickness(80,0);
+            relativeLayout.Padding = new Thickness(80, 0);
 
-            RoundedLabelView.Content = relativeLayout();
-
-        RoundedLabelView.Content = new RoundedLabel
-                {
-                    TextColor = Color.White,
-                    CurvedBackgroundColor = Color.Black,
-                    Text = "RoundedLabel in custom ContentView",
-                    RoundedCornerRadius = 20,
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    HorizontalOptions = LayoutOptions.FillAndExpand,
-                    VerticalOptions = LayoutOptions.FillAndExpand,
-                    VerticalTextAlignment = TextAlignment.Center,
-                    FontSize = 15,
-                    FontAttributes = FontAttributes.Bold
-                };
+            RoundedLabelView.Content = relativeLayout;
         }
     }
 }
